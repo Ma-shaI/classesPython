13da396972e36d3f3f4115a8c5522a1a9a9d6b82
Mary Ivanova <shulakova.masha@yandex.ru>
2023-02-08 13:53:34 +0300

    lesson 07.02 Тема: Парсинг

---
+++ lesson/lesson40.py
@@ -0,0 +1,147 @@
+# import requests
+# r = requests.get('https://ru.wordpress.org/')
+# print(r.headers) # получение заголовка ответа сервера
+# print(r.headers['Content-Type'])
+# print(r.content)
+# print(r.text)
+import csv
+
+# import requests
+# from bs4 import BeautifulSoup
+#
+#
+# def get_html(url):
+#     r = requests.get(url)
+#     return r.text
+#
+#
+# def get_data(html):
+#     soup = BeautifulSoup(html, 'lxml')
+#     p1 = soup.find('header', id="masthead").find('p', class_='site-title').text
+#     return p1
+#
+#
+# def main():
+#     url = 'https://ru.wordpress.org/'
+#     print(get_data(get_html(url)))
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+# import requests
+# from bs4 import BeautifulSoup
+# import re
+# import csv
+#
+#
+# def get_html(url):
+#     r = requests.get(url)
+#     return r.text
+#
+#
+# def refined(s):
+#     return re.sub(r'\D+', '', s)
+#
+#
+# def write_csv(data):
+#     with open('plugins.csv', 'a') as f:
+#         writer = csv.writer(f, lineterminator='\r', delimiter=';')
+#         writer.writerow((data['name'], data['url'], data['rating']))
+#
+#
+# def get_data(html):
+#     soup = BeautifulSoup(html, 'lxml')
+#     p1 = soup.find_all('section', class_='plugin-section')[3]
+#     plugins = p1.find_all('article')
+#     for plugin in plugins:
+#         name = plugin.find('h3').text
+#         url = plugin.find('h3').find('a').get('href')  # ['href']
+#         rating = plugin.find('span', class_='rating-count').find('a').text
+#         r = refined(rating)
+#         data = {'name': name, 'url': url, 'rating': r}
+#         write_csv(data)
+#
+#     # return len(plugins)
+#
+#
+# def main():
+#     url = 'https://ru.wordpress.org/plugins/'
+#     get_data(get_html(url))
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+
+def get_html(url):
+    r = requests.get(url)
+
+    return r.text
+
+
+def refine_cy(s):
+    return s.split()[-1]
+
+
+def write_csv(data):
+    with open('plugins1.csv', 'a') as f:
+        writer = csv.writer(f, lineterminator='\r', delimiter=';')
+        writer.writerow((data['name'], data['url'], data['snippet'], data['cy']))
+
+
+def refine_snippet(s):
+    res = [ord(c) for c in s if ord(c) < 9000]
+    res1 = ''.join([chr(c) for c in res])
+    return res1
+
+
+def get_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+    elements = soup.find_all('article', class_='plugin-card')
+    for el in elements:
+        try:
+            name = el.find('h3').text
+        except ValueError:
+            name = ''
+
+        try:
+            url = el.find('h3').find('a').get('href')
+        except ValueError:
+            url = ''
+
+        try:
+            snippet = el.find('div', 'entry-excerpt').text.strip()
+            text = refine_snippet(snippet)
+        except ValueError:
+            text = ''
+            snippet = ''
+
+        print(text)
+        try:
+            c = el.find('span', class_='tested-with').text.strip()
+            cy = refine_cy(c)
+        except ValueError:
+            cy = ''
+        data = {
+            'name': name,
+            'url': url,
+            'snippet': text,
+            'cy': cy
+        }
+        write_csv(data)
+
+
+def main():
+    for i in range(25):
+        url = f'https://ru.wordpress.org/plugins/browse/blocks/page/8/'
+        get_data(get_html(url))
+
+
+if __name__ == '__main__':
+    main()
