4b616c5b2fdb7078bf3dc037127b862a687d954d
Mary Ivanova <shulakova.masha@yandex.ru>
2023-05-19 07:19:42 +0300

    lesson 18/05

--- fifth/programming_blog/blog/views.py
+++ fifth/programming_blog/blog/views.py
@@ -1,31 +1,30 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import *
+from django.urls import reverse_lazy
+from .forms import *
+from .utils import *
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-menu = [
-    {'title': 'Добавить статью', 'url_name': 'index'},
-    {'title': 'Войти', 'url_name': 'index'},
-]
+class BlogHome(DataMixin, ListView):
 
-
-# Create your views here.
-class BlogHome(ListView):
     model = Blog
     template_name = 'blog/index.html'
     context_object_name = 'posts'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Главная страница'
-        context['menu'] = menu
-        context['cat_selected'] = 0
-        return context
+        # context['title'] = 'Главная страница'
+        # context['menu'] = menu
+        # context['cat_selected'] = 0
+        c_def = self.get_user_context(title='Главная страница')
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Blog.objects.filter(is_published=True).select_related('cat')
 
 
-class ShowPost(DetailView):
+class ShowPost(DataMixin, DetailView):
     model = Blog
     template_name = 'blog/post.html'
     slug_url_kwarg = 'post_slug'
@@ -35,10 +34,13 @@ class ShowPost(DetailView):
         context = super().get_context_data(**kwargs)
         context['title'] = context['post']
         context['menu'] = menu
-        return context
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
 
 
-class BlogCategory(ListView):
+
+class BlogCategory(DataMixin, ListView):
+
     model = Blog
     template_name = 'blog/index.html'
     context_object_name = 'posts'
@@ -46,9 +48,25 @@ class BlogCategory(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
-        context['cat_selected'] = context['posts'][0].cat_id
-        return context
+        # context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        # context['cat_selected'] = context['posts'][0].cat_id
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        c_def = self.get_user_context(title='Категория - ' + str(c.name), cat_selected=c.pk)
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Blog.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True).select_related('cat')
+
+
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'blog/addpage.html'
+    success_url = reverse_lazy('index')
+    login_url = reverse_lazy('index')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['title'] = 'Добавление статьи'
+        # context['menu'] = menu
+        c_def = self.get_user_context(title='Добавление статьи')
+        return dict(list(context.items()) + list(c_def.items()))
